5c1c6984e48fb490012edf16a58cbb44b3441ed4
mortee <mortee@kavemalna.hu>
2024-10-04 18:05:00 +0200

    added exception handling to more areas

--- resources/root/puppeteer-api/puppeteer.js
+++ resources/root/puppeteer-api/puppeteer.js
@@ -19,92 +19,101 @@ const program = require('commander');
 async function scrape({url, selector}, sessionId = "local", returnFullPage = false) {
 
     return new Promise(async (resolve, reject) => {
+        try {
+            if (sessionId) console.log(`[${sessionId}]`, 'starting chrome browser');
+            // see https://github.com/puppeteer/puppeteer/issues/1793#issuecomment-438971272
+            const browser = await puppeteer.launch({
+                executablePath: '/usr/bin/chromium-browser',
+                args: ['--no-sandbox', '--disable-gpu']
+            });
 
-        if (sessionId) console.log(`[${sessionId}]`, 'starting chrome browser');
-        // see https://github.com/puppeteer/puppeteer/issues/1793#issuecomment-438971272
-        const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/chromium-browser',
-            args: ['--no-sandbox', '--disable-gpu']
-        });
-
-        let j = 0;
-        const page = await browser.newPage();
-        if (process.env.USER_AGENT) {
-            page.setUserAgent(process.env.USER_AGENT);
-        }
-
-        async function stop() {
-            if (i) {
-                if (sessionId) console.log(`[${sessionId}]`, 'clearing refresh interval');
-                clearInterval(i);
+            let j = 0;
+            const page = await browser.newPage();
+            if (process.env.USER_AGENT) {
+                page.setUserAgent(process.env.USER_AGENT);
             }
-            if (sessionId) console.log(`[${sessionId}]`, 'closing chrome browser');
-            await page.close();
-            await browser.close();
-        }
 
-        async function check() {
-            try {
-                let elements = await page.$$(selector);
-                if (elements.length) {
-                    if (sessionId) console.log(`[${sessionId}]`, `element with selector: '${selector}' appeared, resolving content`);
-                    if (returnFullPage) {
-                        resolve(await page.content());
-                    } else {
-                        // received a lot of fatal errors trying to access closed pages:
-                        // Error: Protocol error (Runtime.callFunctionOn): Target closed.
-                        // see https://stackoverflow.com/a/73334739
-                        // the method below seems to circumvent this problem
-                        const elementContentsList = [];
-                        await elements.reduce(
-                            async (item, element) => {
-                                await item;
-                                elementContentsList.push(await page.evaluate(el => el.outerHTML, element));
-                            },
-                            Promise.resolve()
-                        );
-                        resolve(elementContentsList.join("\n"));
+            async function stop() {
+                if (i) {
+                    if (sessionId) console.log(`[${sessionId}]`, 'clearing refresh interval');
+                    clearInterval(i);
+                }
+                if (sessionId) console.log(`[${sessionId}]`, 'closing chrome browser');
+                try {
+                    await page.close();
+                    await browser.close();
+                } catch (e) {
+                    console.error(`[${sessionId}]`, `puppeteer error: ${e.message}`);
+                }
+            }
+
+            async function check() {
+                try {
+                    let elements = await page.$$(selector);
+                    if (elements.length) {
+                        if (sessionId) console.log(`[${sessionId}]`, `element with selector: '${selector}' appeared, resolving content`);
+                        if (returnFullPage) {
+                            resolve(await page.content());
+                        } else {
+                            // received a lot of fatal errors trying to access closed pages:
+                            // Error: Protocol error (Runtime.callFunctionOn): Target closed.
+                            // see https://stackoverflow.com/a/73334739
+                            // the method below seems to circumvent this problem
+                            const elementContentsList = [];
+                            await elements.reduce(
+                                async (item, element) => {
+                                    await item;
+                                    elementContentsList.push(await page.evaluate(el => el.outerHTML, element));
+                                },
+                                Promise.resolve()
+                            );
+                            resolve(elementContentsList.join("\n"));
+                        }
+                        await stop();
+                    } else if (++j === 60) { // 60 secs timeout
+                        if (sessionId) console.log(`[${sessionId}]`, `element with selector: '${selector}' didn't appear, timeout`);
+                        reject([404, 'didn\'t appear']);
+                        await stop();
                     }
-                    await stop();
-                } else if (++j === 60) { // 60 secs timeout
-                    if (sessionId) console.log(`[${sessionId}]`, `element with selector: '${selector}' didn't appear, timeout`);
-                    reject([404, 'didn\'t appear']);
+                } catch(e) {
+                    console.error(`[${sessionId}]`, `puppeteer error: ${e.message}`);
+                    reject([500, `puppeteer error: ${e.message}`]);
                     await stop();
                 }
-            } catch(e) {
-                if (sessionId) console.error(`[${sessionId}]`, `puppeteer error: ${e.message}`);
-                reject([500, `puppeteer error: ${e.message}`]);
-                await stop();
+
             }
 
-        }
+            let i = null, k = null;
+            page.once('load', async () => {
+                if (k) {
+                    if (sessionId) console.log(`[${sessionId}]`, 'clearing wait for page load timeout');
+                    clearTimeout(k);
+                }
 
-        let i = null, k = null;
-        page.once('load', async () => {
-            if (k) {
-                if (sessionId) console.log(`[${sessionId}]`, 'clearing wait for page load timeout');
-                clearTimeout(k);
+                if (selector) {
+                    if (sessionId) console.log(`[${sessionId}]`, `page loaded; looking for selector: '${selector}'. setting 1000 ms refresh interval`);
+                    i = setInterval(check, 1000);
+                } else {
+                    if (sessionId) console.log(`[${sessionId}]`, `page loaded; resolving content immediately`);
+                    resolve(await page.content());
+                    await stop();
+                }
+            });
+
+            if (sessionId) console.log(`[${sessionId}]`, `going to: ${url}`);
+            try {
+                await page.goto(url);
+            } catch (e) {
+                if (sessionId) console.error(`[${sessionId}]`, `error: ${e.message}, but continue to wait for onload yet another time`);
+                k = setTimeout(async () => {
+                    reject([503, 'pageload timeout']);
+                    await stop();
+                }, 30000);
             }
-
-            if (selector) {
-                if (sessionId) console.log(`[${sessionId}]`, `page loaded; looking for selector: '${selector}'. setting 1000 ms refresh interval`);
-                i = setInterval(check, 1000);
-            } else {
-                if (sessionId) console.log(`[${sessionId}]`, `page loaded; resolving content immediately`);
-                resolve(await page.content());
-                await stop();
-            }
-        });
-
-        if (sessionId) console.log(`[${sessionId}]`, `going to: ${url}`);
-        try {
-            await page.goto(url);
-        } catch (e) {
-            if (sessionId) console.error(`[${sessionId}]`, `error: ${e.message}, but continue to wait for onload yet another time`);
-            k = setTimeout(async () => {
-                reject([503, 'pageload timeout']);
-                await stop();
-            }, 30000);
+        } catch(e) {
+            console.error(`[${sessionId}]`, `puppeteer error: ${e.message}`);
+            reject([500, `puppeteer error: ${e.message}`]);
+            await stop();
         }
 
     });
